2814f7b3afe203fbd565343eaad9950f53451593
iamismail <ismailmbarouk@gmail.com>
2024-09-05 02:27:01 -0700

    saved

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'height': '36rem',
+        'heights': '64rem',
+        'bheight': '1px'
+      },
+      width: {
+        'width': '20px',
+      },
+      spacing: {
+        'top': '22rem',
+        'marginb': '30rem',
+      }
+    },
   },
   plugins: [],
 }
